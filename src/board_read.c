27d57b923d2fe6ce160735d711d979e426206eac
OlyaKnyaz <olya.cnyazeva2015@yandex.ru>
2020-05-11 19:40:36 +0700

    Исправление board_read.c Добавление перемещения

--- src/board_read.c
+++ src/board_read.c
@@ -9,97 +9,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fig_move(char board[8][8], int move[])
-{
-    char temp = board[move[1]][move[0]];
-    board[move[1]][move[0]] = ' ';
-    board[move[3]][move[2]] = temp;
-}
+#include "board_read.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int check_move(char deck[8][8], int move[])
+void board_read(char str[])
 {
-    switch (deck[move[1]][move[0]]) {
-        case 'p':
-            return pawn(deck, move, 0);
-        case 'P':
-            return pawn(deck, move, 1);
-        case 'r':
-            fig_move(deck, move);
-            return 0;
-        case 'R':
-            fig_move(deck, move);
-            return 0;
-        case 'n':
-            fig_move(deck, move);
-            return 0;
-        case 'N':
-            fig_move(deck, move);
-            return 0;
-        case 'b':
-            fig_move(deck, move);
-            return 0;
-        case 'B':
-            fig_move(deck, move);
-            return 0;
-        case 'q':
-            fig_move(deck, move);
-            return 0;
-        case 'Q':
-            fig_move(deck, move);
-            return 0;
-        case 'k':
-            fig_move(deck, move);
-            return 0;
-        case 'K':
-            fig_move(deck, move);
-            return 0;
-        default:
-            return 1;
-    }
-}
-
-int pawn(char deck[8][8], int move[], char tm)
-{
-    if (tm == 1) {
-        if ((move[2] == move[0] && deck[move[3]][move[2]] == ' '
-             && (move[3] == move[1] - 1 || (move[1] == 6 && move[3] == 4)))
-            || (move[3] == move[1] - 1
-                && (move[2] == move[0] + 1 || move[2] == move[0] - 1)
-                && deck[move[3]][move[2]] >= 'a'
-                && deck[move[3]][move[2]] <= 'z')) {
-            fig_move(deck, move);
-            return 0;
-        } else
-            return 1;
-    }
-    if (tm == 0) {
-        if ((move[2] == move[0] && deck[move[3]][move[2]] == ' '
-             && (move[3] == move[1] + 1 || (move[1] == 1 && move[3] == 3)))
-            || (move[3] == move[1] + 1
-                && (move[2] == move[0] + 1 || move[2] == move[0] - 1)
-                && deck[move[3]][move[2]] >= 'A'
-                && deck[move[3]][move[2]] <= 'Z')) {
-            fig_move(deck, move);
-            return 0;
-        } else
-            return 1;
-    }
-    return 0;
-}
-
-void board(char deck[8][8])
-{
-    char* str = calloc(7, sizeof(char));
-    int flag = 1;
+    int flag;
     do {
-        board_read(str);
-        int move[4];
-        reformat_input(str, move);
-
-        flag = check_move(deck, move);
+        scanf("%6s", str);
+        flag = (check_input(str));
         if (flag) {
-            printf("Incorrect turn, try again\n");
+            printf("Incorrect input, try again\n");
         }
     } while (flag);
-    free(str);
+}
+
+int check_input(char str[])
+{
+    if (str[0] < 'a'  str[0] > 'h'  str[1] < '1'  str[1] > '8'
+    str[2] != '-'  str[3] < 'a'  str[3] > 'h'  str[4] < '1'
+    str[4] > '8' || str[5] != '\0') {
+        return 1;
+    }
+
+    else {
+        return 0;
+    }
+}
+void reformat_input(char input[], int move[4])
+{
+    char border = 8; //граница
+    move[0] = input[0] - 'a';
+    move[1] = border - (input[1] - '0');
+    move[2] = input[3] - 'a';
+    move[3] = border - (input[4] - '0');
 }
