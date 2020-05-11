802ac33481ee029731802c2d0e4a5d9e991dda09
OlyaKnyaz <olya.cnyazeva2015@yandex.ru>
2020-05-11 19:30:37 +0700

    добавила функцию вывда доски

--- src/ board_print_plain.c
+++ src/ board_print_plain.c
@@ -3,3 +3,18 @@
 //
 
 #include " board_print_plain.h"
+
+#include "board_print_plain.h"
+#include <stdio.h>
+
+void board_print(char board[8][8])
+{
+    for (int i = 0; i < 8; i++) {
+        printf("%d ", 8 - i);
+        for (int j = 0; j < 8; j++) {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("  A B C D E F G H\n");
+}
