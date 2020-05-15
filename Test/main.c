0b8645877cba17b2182c3ad68a30061bddbcb133
OlyaKnyaz <olya.cnyazeva2015@yandex.ru>
2020-05-16 01:02:56 +0700

    Исправление

--- Test/main.c
+++ Test/main.c
@@ -7,64 +7,64 @@
 #include <board_read.h>
 CTEST(Syntax, Correct)
 {
-int RES = check_input("b2-b5");
-int OZH = 0;
-ASSERT_EQUAL(OZH, RES);
+int result = check_input("b2-b5");
+int expected = 0;
+ASSERT_EQUAL(expected, result);
 }
 
 CTEST(Syntax, Incorrect)
 {
-reformat_input( VVOD, HOD);
-int RES = check_input("a2-a9");
-int OZH = 1;
-ASSERT_EQUAL(OZH, RES);
+reformat_input( input, move);
+int result = check_input("a2-a9");
+int expected = 1;
+ASSERT_EQUAL(expected, result);
 }
 
 CTEST(pawn, Correct)
 {
 char deck[8][8];
 board_start(deck);
-char VVOD[] = "a7-a5";
-int HOD[4];
-int RES = pawn(deck, HOD, 0);
-int OZH = 0;
-ASSERT_EQUAL(OZH, RES);
+char input[] = "a7-a5";
+int move[4];
+int result = pawn(deck, move, 0);
+int expected = 0;
+ASSERT_EQUAL(expected, result);
 }
 
 CTEST(pawn, Incorrect)
 {
 char deck[8][8];
 board_start(deck);
-char VVOD[] = "a7-b6";
-int HOD[4];
-reformat_input( VVOD, HOD);
-int RES = pawn(deck, HOD, 0);
-int OZH = 1;
-ASSERT_EQUAL(OZH, RES);
+char input[] = "a7-b6";
+int move[4];
+reformat_input( input, move);
+int result = pawn(deck, move, 0);
+int expected = 1;
+ASSERT_EQUAL(expected, result);
 }
 
 CTEST(check_move, Correct)
 {
 char deck[8][8];
 board_start(deck);
-char VVOD[] = "c7-c6";
-int HOD[4];
-reformat_input( VVOD, HOD);
-int RES = check_move(deck, HOD);
-int OZH = 0;
-ASSERT_EQUAL(OZH, RES);
+char input[] = "c7-c6";
+int move[4];
+reformat_input( input, move);
+int result = check_move(deck, move);
+int expected = 0;
+ASSERT_EQUAL(expected, result);
 }
 
 CTEST(check_move, Incorrect)
 {
 char deck[8][8];
 board_start(deck);
-char VVOD[] = "e5-e6";
-int HOD[4];
-reformat_input( VVOD, HOD);
-int RES = check_move(deck, HOD);
-int OZH = 1;
-ASSERT_EQUAL(OZH, RES);
+char input[] = "e5-e6";
+int move[4];
+reformat_input( input, move);
+int result = check_move(deck, move);
+int expected = 1;
+ASSERT_EQUAL(expected, result);
 }
 
 int main(int argc, const char** argv)
